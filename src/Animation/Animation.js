68e8a8473156189559057255a447c719d591a9ff
Andy Evans <andrewevans0@outlook.com>
2018-03-09 19:35:06 +0000

    added basic syntax - broken

--- src/Animation/Animation.js
+++ src/Animation/Animation.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import './Animation.css';
 
+function main() {
+  const canvas = document.querySelector("#glCanvas");
+  // Initialize the GL context
+  const gl = canvas.getContext("webgl");
+
+  // Only continue if WebGL is available and working
+  if (!gl) {
+    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
 class Animation extends Component {
   render() {
+    main();
+
     return (
-      <div className="App">
-        test
-      </div>
+      <canvas id="glCanvas" width={640} height={480}></canvas>
     );
   }
 }
